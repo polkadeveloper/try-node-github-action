150802a9bc63c679a0e31af2ae27b4f8daf31f05
alvarob27 <alvarobarcena27@gmail.com>
2024-02-21 18:03:55 +0100

    Actualización de código: cambios en el archivo git diff

---
+++ app.js
@@ -0,0 +1,15 @@
+import fs from 'fs'
+
+fs.readFile('file.txt', 'utf8', (err, data) => {
+  const lines = data.split('\n')
+
+  console.log(lines)
+
+  lines.forEach((line) => {
+    console.log(line)
+  })
+
+  if (err) {
+    console.log(err)
+  }
+})
