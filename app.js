fb9db00503a4f860a1ce242a2b5127d352208a6a
alvarob27 <alvarobarcena27@gmail.com>
2024-02-22 15:08:42 +0100

    Añadir funciones y corregir errores en app.js y app.spec.js, actualizar .gitignore y server.js

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
 // sum.js
-export function sum(a, b) {
-  return a + b;
+export function sum (a, b) {
+  return a + b
 }
 // --> Extract the last index of an array
-export function lastIndex(array) {
-  return array.at(-1);
+export function lastIndex (array) {
+  return array.at(-1)
 }
 
-export function isEven(n) {
-  return n % 2 === 0;
-} 
-
-export function isOdd(n) {
-  return n % 2 !== 0;
+export function isEven (n) {
+  return n % 2 === 0
 }
 
-export function isPrime(n) {
-  if (n <= 1) return false;
-  if (n === 2) return true;
-  if (n % 2 === 0) return false;
+export function isOdd (n) {
+  return n % 2 !== 0
+}
+
+export function isPrime (n) {
+  if (n <= 1) return false
+  if (n === 2) return true
+  if (n % 2 === 0) return false
   for (let i = 3; i <= Math.sqrt(n); i += 2) {
-    if (n % i === 0) return false;
+    if (n % i === 0) return false
   }
-  return true;
+  return true
 }
 
-export function maxDistance(movements) {
+export function maxDistance (movements) {
+  let distance = 0
+  let comodines = 0
 
-  let distance = 0;
-  let comodines = 0;
-
-  for(let signo of movements) {
-      if(signo === '>') {
-          distance++;
-      } else if(signo === '<') {
-          distance--;
-      } else {
-          comodines++;
-      }
+  for (const signo of movements) {
+    if (signo === '>') {
+      distance++
+    } else if (signo === '<') {
+      distance--
+    } else {
+      comodines++
+    }
   }
 
-  return Math.abs(distance) + comodines;
+  return Math.abs(distance) + comodines
 }
 // Path: app.spec.js
